08e916c957a4c692c5c7014e535a8950ac4b1ad9
Gulshan <gullu.kr22@gmail.com>
2021-06-01 21:44:30 +0530

    correction

--- EventApplicationCore/wwwroot/js/slider.js
+++ EventApplicationCore/wwwroot/js/slider.js
@@ -1,37 +1,46 @@
-﻿const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImage = document.querySelector('.carousel-slide img');
+﻿var slideIndex = 1;
+showSlides(slideIndex);
 
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-let counter = 1;
-const size = carouselImage[0].clientWidth;
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-carouselSlide.stylr.transform = 'tanslateX(' + (-size * counter) + 'px)';
-
-nextBtn.addEventListener('click', () => {
-    if (counter >= carouselImage.length-1) return;
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-prevBtn.addEventListener('click', () => {
-    if (counter <= 0) return;
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-carouselSlide.addEventListener('transitionend', () => {
-    if (carouselImage[counter].id === 'lastClone') {
-        carouselSlide.style.transform = 'none';
-        counter = carouselImage.length - 2;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    if (carouselImage[counter].id === 'firstClone') {
-        carouselSlide.style.transform = 'none';
-        counter = carouselImage.length - counter;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-});
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+// Auto Slide   if you need auto slide ,remove the commit "//"
+
+
+
+//var slideIndex = 0;
+//showSlides();
+
+//function showSlides() {
+    //var i;
+  //  var slides = document.getElementsByClassName("mySlides");
+   // for (i = 0; i < slides.length; i++) {
+   //     slides[i].style.display = "none";
+   // }
+  //  slideIndex++;
+   // if (slideIndex > slides.length) { slideIndex = 1 }
+   // slides[slideIndex - 1].style.display = "block";
+   // setTimeout(showSlides, 2000); // Change image every 2 seconds
+//}
